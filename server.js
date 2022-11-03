9f0b3f9b605eba3d7362089ec0c334af0a1bc7dd
Abhiroop25902 <abhiroop.m25902@gmail.com>
2022-11-04 01:06:45 +0530

    [ADD] added a health check

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const users = require('./routes/users')
 const matchmaking = require('./routes/matchmaking')
+const health = require('./routes/health')
 require('dotenv').config()
 
 const app = express()
@@ -10,6 +11,7 @@ app.use(express.json()); //for json support
 app.use(cors())
 app.use('/users', users)
 app.use('/matchmaking', matchmaking)
+app.use('/health', health)
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port,
